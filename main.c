43f4ce836a76141cbb7574d8d42455ad301666bf
Daniel Calderón <daniel.calderon.fernandez@udla.edu.ec>
2023-07-05 01:02:02 -0500

    Declaración variables de caracter

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main(void){
 
 int voto, cand1 = 0, cand2 = 0, cand3 = 0, cand4 = 0, cand5 = 0;
 float totalVotos, pCand1, pCand2, pCand3, pCand4, pCand5;
+char nCand1[20], nCand2[20], nCand3[20], nCand4[20], nCand5[20];
 
 do
 {
