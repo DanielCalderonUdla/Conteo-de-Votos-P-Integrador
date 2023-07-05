a1ea2ce76666f637291fc1337f0de32c032b63b4
Daniel Calderón <daniel.calderon.fernandez@udla.edu.ec>
2023-07-05 00:42:51 -0500

    Declaración de variables y librerias

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+
+int voto, cand1 = 0, cand2 = 0, cand3 = 0, cand4 = 0, cand5 = 0;
+float totalVotos, pCand1, pCand2, pCand3, pCand4, pCand5;
+
+
+
+
+return 0;
+
+}
