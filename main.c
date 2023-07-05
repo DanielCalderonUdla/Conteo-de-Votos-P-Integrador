35993013e1a774c69b59b1b481dcc8f8d356490a
Daniel Calderón <daniel.calderon.fernandez@udla.edu.ec>
2023-07-05 00:54:25 -0500

    Añadido el conteo de votos con IF

--- main.c
+++ main.c
@@ -11,7 +11,32 @@ do
     printf("Ingrese el candidato que recibió un voto (1, 2, 3, 4, 5) (Terminar = 0))\n");
     scanf("%d", &voto);
 
-
+    if (voto == 1)
+    {
+        cand1++; 
+        printf("Se ha registrado un voto para el Candidato 1\n");
+    } else if (voto == 2)
+    {
+        cand2++;
+        printf("Se ha registrado un voto para el Candidato 2\n");
+    } else if (voto == 3)
+    {
+        cand3++;
+        printf("Se ha registrado un voto para el Candidato 3\n");
+    } else if (voto == 4)
+    {
+        cand4++;
+        printf("Se ha registrado un voto para el Candidato 4\n");
+    } else if (voto == 5)
+    {
+        cand5++;
+        printf("Se ha registrado un voto para el Candidato 5\n");
+    } else if (voto == 0)
+    {
+        printf("Gracias por registrar todos los votos, estos son los resultados:\n");
+    } else {
+        printf("Ingrese un candidato valido\n");
+    }
 
 } while (voto != 0);
 
