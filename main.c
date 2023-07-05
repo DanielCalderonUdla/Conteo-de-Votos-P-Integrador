6cc82164ba311705b747d2079db6345176ccc2a4
Daniel Calderón <daniel.calderon.fernandez@udla.edu.ec>
2023-07-05 00:52:01 -0500

    Añadido Do-While y el ingreso de votos

--- main.c
+++ main.c
@@ -6,8 +6,14 @@ int main(void){
 int voto, cand1 = 0, cand2 = 0, cand3 = 0, cand4 = 0, cand5 = 0;
 float totalVotos, pCand1, pCand2, pCand3, pCand4, pCand5;
 
+do
+{
+    printf("Ingrese el candidato que recibió un voto (1, 2, 3, 4, 5) (Terminar = 0))\n");
+    scanf("%d", &voto);
 
 
+
+} while (voto != 0);
 
 return 0;
 
